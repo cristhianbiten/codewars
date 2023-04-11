669dce6a3a81bc01a709137b42b469a825e24ae4
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-04-11 15:27:49 -0300

    Add new exercise(Banjo)

---
+++ src/areYouPlayingBanjo/Banjo.java
@@ -0,0 +1,12 @@
+package areYouPlayingBanjo;
+
+public class Banjo {
+
+    public static String areYouPlayingBanjo(String name) {
+
+        if (name.toLowerCase().startsWith("r")) {
+            return name + " plays banjo";
+        }
+        return name + " does not play banjo";
+    }
+}
