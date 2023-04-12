1b3e87eb6cb931e79710c1d6d7216cde9c6d1b23
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-04-12 14:27:30 -0300

    Add new exercise(Help Alex)

---
+++ src/keepUpTheHoop/HelpAlex.java
@@ -0,0 +1,8 @@
+package keepUpTheHoop;
+
+public class HelpAlex {
+        public static String hoopCount(int n){
+            return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+
+    }
+}
