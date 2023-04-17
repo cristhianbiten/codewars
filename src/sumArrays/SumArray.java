0275f92cfd66bd597726e7f84075baa16538ddf3
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-04-17 15:10:56 -0300

    Add new exercise(SumArray)

---
+++ src/sumArrays/SumArray.java
@@ -0,0 +1,15 @@
+package sumArrays;
+
+public class SumArray {
+
+    public static double sum(double[] numbers) {
+
+        double sum = 0;
+
+        for (double n : numbers) {
+            sum += n;
+        }
+
+        return sum;
+    }
+}
