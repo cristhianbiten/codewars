d7c2e1375494804fbc7a56ce1b488505482726b5
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2023-04-20 11:50:57 -0300

    Add new exercise - contar charat

---
+++ src/allStarCode/CodeWars.java
@@ -0,0 +1,13 @@
+package allStarCode;
+
+public class CodeWars {
+    public static int strCount(String str, char letter) {
+        int count=0;
+        for (int i=0;i<str.length();i++){
+            if(str.charAt(i)==letter){
+                count++;
+            }
+        }
+        return count;
+    }
+}
